5402d2a5e3aef359d93b6bf82c20a6544a82cba4
Dark-Matter7232 <kerneldeveloper7232@gmail.com>
2022-04-03 21:27:48 +0530

    update scripts

--- build.sh
+++ build.sh
@@ -1,69 +1,134 @@
-#!/usr/bin/env bash
-# SPDX-License-Identifier: GPL-3.0-or-later
-# Copyright (C) 2018 Raphiel Rollerscaperers (raphielscape)
-# Copyright (C) 2018 Rama Bndan Prakoso (rama982)
-# Android Kernel Build Script
+#!/bin/bash
 
-yellow='\033[0;33m'
-white='\033[0m'
-red='\033[0;31m'
-gre='\e[0;32m'
+# Initialize variables
 
-# Main environtment
-KERNEL_DIR=$PWD
-KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz
-ZIP_DIR=$KERNEL_DIR/../anykernel3
-CONFIG=vendor/hanoip_defconfig
-CROSS_COMPILE="aarch64-linux-android-"
-CROSS_COMPILE_ARM32="arm-linux-androideabi-"
-PATH="${KERNEL_DIR}/../cl12/bin:${KERNEL_DIR}/../aarch64-linux-android-4.9/bin:${KERNEL_DIR}/../arm-linux-androideabi-4.9/bin:${PATH}"
+BOLD='\033[1m'
+GRN='\033[01;32m'
+CYAN='\033[0;36m'
+YELLOW='\033[1;33m'
+RED='\033[01;31m'
+RST='\033[0m'
+ORIGIN_DIR=$(pwd)
+BUILD_PREF_COMPILER='clang'
+BUILD_PREF_COMPILER_VERSION='proton'
+TOOLCHAIN=$(pwd)/build-shit/toolchain
+IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz
+# export environment variables
+export_env_vars() {
+    export KBUILD_BUILD_USER=Const
+    export KBUILD_BUILD_HOST=Coccinelle
 
-# Export
-export LOCALVERSION=-v1
+	export ARCH=arm64
+	export SUBARCH=arm64
+	export HEADER_ARCH=arm64
+    export CROSS_COMPILE=aarch64-linux-gnu-
+    export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 
-export ARCH=arm64
-export SUBARCH=arm64
-export HEADER_ARCH=arm64
-export CROSS_COMPILE
-export CROSS_COMPILE_ARM32
+    # CCACHE
+    export USE_CCACHE=1
+    export PATH="/usr/lib/ccache/bin/:$PATH"
+    export CCACHE_SLOPPINESS="file_macro,locale,time_macros"
+    export CCACHE_NOHASHDIR="true"
+    export CC=${BUILD_PREF_COMPILER}
+}
 
-export KBUILD_BUILD_HOST="mint"
-export KBUILD_BUILD_USER="cool585"
+script_echo() {
+    echo "  $1"
+}
+exit_script() {
+    kill -INT $$
+}
+add_deps() {
+    echo -e "${CYAN}"
+    if [ ! -d $(pwd)/build-shit ]
+    then
+        script_echo "Create build-shit folder"
+        mkdir $(pwd)/build-shit
+    fi
 
-# Build start
-Start=$(date +"%s")
+    if [ ! -d $(pwd)/build-shit/toolchain ]
+    then
+        script_echo "Downloading proton-clang...."
+        cd build-shit
+        script_echo $(wget -q https://github.com/kdrag0n/proton-clang/archive/refs/tags/20201212.tar.gz -O clang.tar.gz);
+        bsdtar xf clang.tar.gz
+        rm -rf clang.tar.gz
+        mv proton-clang* toolchain
+        cd ../
+    fi
+    verify_toolchain_install
+}
+verify_toolchain_install() {
+    script_echo " "
+    if [[ -d "${TOOLCHAIN}" ]]; then
+        script_echo "I: Toolchain found at default location"
+        export PATH="${TOOLCHAIN}/bin:$PATH"
+        export LD_LIBRARY_PATH="${TOOLCHAIN}/lib:$LD_LIBRARY_PATH"
+    else
+        script_echo "I: Toolchain not found"
+        script_echo "   Downloading recommended toolchain at ${TOOLCHAIN}..."
+        add_deps
+    fi
+}
+build_kernel_image() {
+    cleanup
+    script_echo " "
+    echo -e "${GRN}"
+    read -p "Write the Kernel version: " KV
+    echo -e "${YELLOW}"
+    script_echo 'Building CosmicFresh Kernel For M21'
+    make -C $(pwd) CC=${BUILD_PREF_COMPILER} AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip -j$((`nproc`+1)) LOCALVERSION="—CosmicFresh-R$KV-hanoip" vendor/hanoip_defconfig 2>&1 | sed 's/^/     /'
+    make -C $(pwd) CC=${BUILD_PREF_COMPILER} AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip -j$((`nproc`+1)) LOCALVERSION="—CosmicFresh-R$KV-hanoip" 2>&1 | sed 's/^/     /'
+    SUCCESS=$?
+    echo -e "${RST}"
 
-make O=out $CONFIG
-make -j$(nproc --all) O=out \
-                      ARCH=arm64 \
-                      CC=clang \
-CLANG_TRIPLE=aarch64-linux-gnu- \
-CROSS_COMPILE=aarch64-linux-android- | tee build.log
-
-exit_code=$?
-End=$(date +"%s")
-Diff=$(($End - $Start))
-
-if [ -f $KERN_IMG ]; then
-	mkdir -p $ZIP_DIR
-	cp -f ./out/arch/arm64/boot/Image.gz $ZIP_DIR/Image.gz
-	cp -f ./out/arch/arm64/boot/dts/qcom/sdmmagpie-hanoi-base.dtb $ZIP_DIR/dtb
-	cp -f ./out/arch/arm64/boot/dtbo.img $ZIP_DIR/dtbo.img
-	which avbtool &>/dev/null && python2 `which avbtool` add_hash_footer \
+    if [ $SUCCESS -eq 0 ] && [ -f "$IMAGE" ]
+    then
+        echo -e "${GRN}"
+        script_echo "------------------------------------------------------------"
+        script_echo "Compilation successful..."
+        script_echo "Image can be found at out/arch/arm64/boot/Image.gz"
+        script_echo  "------------------------------------------------------------"
+        build_flashable_zip
+    elif [ $SUCCESS -eq 130 ]
+    then
+        echo -e "${RED}"
+        script_echo "------------------------------------------------------------"
+        script_echo "Build force stopped by the user."
+        script_echo "------------------------------------------------------------"
+        echo -e "${RST}"
+    elif [ $SUCCESS -eq 1 ]
+    then
+        echo -e "${RED}"
+        script_echo "------------------------------------------------------------"
+        script_echo "Compilation failed..check build logs for errors"
+        script_echo "------------------------------------------------------------"
+        echo -e "${RST}"
+        cleanup
+    fi
+}
+build_flashable_zip() {
+    script_echo " "
+    script_echo "I: Building kernel image..."
+    echo -e "${GRN}"
+    cp $(pwd)/out/arch/arm64/boot/{Image.gz,dtbo.img} CosmicFresh/
+	cp $(pwd)/out/arch/arm64/boot/dts/qcom/sdmmagpie-hanoi-base.dtb CosmicFresh/dtb
+		which avbtool &>/dev/null && python3 `which avbtool` add_hash_footer \
 		--partition_name dtbo \
 		--partition_size $((32 * 1024 * 1024)) \
-		--image $ZIP_DIR/dtbo.img
-	echo -e "$gre << Build completed in $(($Diff / 60)) minutes and $(($Diff % 60)) seconds >> \n $white"
-else
-	echo -e "$red << Failed to compile Image.gz-dtb, fix the errors first >>$white"
-	exit $exit_code
-fi
+		--image $(pwd)/CosmicFresh/dtbo.img
+    cd $(pwd)/CosmicFresh/
+    zip -r9 "CosmicFresh-R$KV-hanoip.zip" anykernel.sh META-INF tools version Image.gz dtb dtbo.img
+    rm -rf {Image.gz,dtb,dtbo.img}
+    cd ../
+}
 
-cd $ZIP_DIR
-make clean &>/dev/null
-make normal &>/dev/null
-rm -rf hanoip.zip
-zip -r9 hanoip.zip * -x .git README.md *placeholder
-echo "Flashable zip generated under $ZIP_DIR."
-cd ..
-# Build end
+cleanup() {
+    cd $(pwd)/CosmicFresh/
+    rm -rf {Image.gz,*.zip,dtb,dtbo.img}
+    cd ../
+    rm -rf $(pwd)/out/arch/arm64/boot/*
+}
+add_deps
+export_env_vars
+build_kernel_image
